2b14733c532b4eb3eb3f2d2dc9267b2bc431f44d
Mike <nicholasfierke97@gmail.com>
2023-02-21 15:30:51 -0500

    set up schema table for roles

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,18 @@
 DROP DATABASE employee_db
 CREATE DATABASE employee_db
 USE DATABASE employee_db
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    department_name VARCHAR(100) NOT NULL
+)
+
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+)
