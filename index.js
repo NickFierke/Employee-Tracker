e879ab964e2642c7fcb972848232cde09b88e7e6
Mike <nicholasfierke97@gmail.com>
2023-02-21 15:21:48 -0500

    set up view functions

--- index.js
+++ index.js
@@ -3,14 +3,9 @@ const logo = require("asciiart-logo");
 const db = require("./db");
 require("console.table");
 
-init();
-
-
 function init() {
   const logoText = logo({ name: "Employee Manager" }).render();
-
   console.log(logoText);
-
   loadMainPrompts();
 }
 
@@ -18,122 +13,91 @@ function loadMainPrompts() {
   prompt([
     {
       type: "list",
-      name: "choice",
+      name: "choices",
       message: "What would you like to do?",
       choices: [
         {
           name: "View All Employees",
-          value: "VIEW_EMPLOYEES"
-        },
-        {
-          name: "View All Employees By Department",
-          value: "VIEW_EMPLOYEES_BY_DEPARTMENT"
-        },
-        {
-          name: "View All Employees By Manager",
-          value: "VIEW_EMPLOYEES_BY_MANAGER"
+          value: "VIEWEMPLOYEES",
         },
         {
           name: "Add Employee",
-          value: "ADD_EMPLOYEE"
-        },
-        {
-          name: "Remove Employee",
-          value: "REMOVE_EMPLOYEE"
-        },
-        {
-          name: "Update Employee Role",
-          value: "UPDATE_EMPLOYEE_ROLE"
-        },
-        {
-          name: "Update Employee Manager",
-          value: "UPDATE_EMPLOYEE_MANAGER"
+          value: "ADDEMPLOYEE",
         },
         {
           name: "View All Roles",
-          value: "VIEW_ROLES"
+          value: "VIEWROLES",
         },
         {
           name: "Add Role",
-          value: "ADD_ROLE"
-        },
-        {
-          name: "Remove Role",
-          value: "REMOVE_ROLE"
+          value: "ADDROLE",
         },
         {
           name: "View All Departments",
-          value: "VIEW_DEPARTMENTS"
+          value: "VIEWDEPARTMENTS",
         },
         {
           name: "Add Department",
-          value: "ADD_DEPARTMENT"
-        },
-        {
-          name: "Remove Department",
-          value: "REMOVE_DEPARTMENT"
-        },
-        {
-          name: "View Total Utilized Budget By Department",
-          value: "VIEW_UTILIZED_BUDGET_BY_DEPARTMENT"
+          value: "ADDDEPARTMENT",
         },
         {
           name: "Quit",
-          value: "QUIT"
-        }
-      ]
-    }
-  ]).then(res => {
-    let choice = res.choice;
-  
-    switch (choice) {
-      case "VIEW_EMPLOYEES":
-        viewEmployees();
+          value: "QUIT",
+        },
+      ],
+    },
+  ]).then((res) => {
+    switch (res.choices) {
+      case "VIEWEMPLOYEES":
+        viewAllEmployees();
         break;
-      case "VIEW_EMPLOYEES_BY_DEPARTMENT":
-        viewEmployeesByDepartment();
-        break;
-      case "VIEW_EMPLOYEES_BY_MANAGER":
-        viewEmployeesByManager();
-        break;
-      case "ADD_EMPLOYEE":
+      case "ADDEMPLOYEE":
         addEmployee();
         break;
-      case "REMOVE_EMPLOYEE":
-        removeEmployee();
+      case "VIEWDEPARTMENTS":
+        viewAllDepartments();
         break;
-      case "UPDATE_EMPLOYEE_ROLE":
-        updateEmployeeRole();
-        break;
-      case "UPDATE_EMPLOYEE_MANAGER":
-        updateEmployeeManager();
-        break;
-      case "VIEW_DEPARTMENTS":
-        viewDepartments();
-        break;
-      case "ADD_DEPARTMENT":
+      case "ADDDEPARTMENT":
         addDepartment();
         break;
-      case "REMOVE_DEPARTMENT":
-        removeDepartment();
+      case "VIEWROLES":
+        viewAllRoles();
         break;
-      case "VIEW_UTILIZED_BUDGET_BY_DEPARTMENT":
-        viewUtilizedBudgetByDepartment();
-        break;
-      case "VIEW_ROLES":
-        viewRoles();
-        break;
-      case "ADD_ROLE":
+      case "ADDROLE":
         addRole();
-        break;
-      case "REMOVE_ROLE":
-        removeRole();
         break;
       default:
         quit();
     }
-  }
-  )
+  });
 }
 
+// VIEW FUNCTIONS
+function viewAllEmployees() {
+  const logoText = logo({ name: "View All Employees" }).render();
+  console.log(logoText);
+  db.query("SELECT * FROM employee", function (res, err) {
+    if (err) throw err;
+    console.table(res);
+  });
+}
 
+function viewAllRoles() {
+  const logoText = logo({ name: "View All Roles" }).render();
+  console.log(logoText);
+  db.query("SELECT * FROM roles", function (res, err) {
+    if (err) throw err;
+    console.table(res);
+  });
+}
+
+function viewAllDepartments() {
+  const logoText = logo({ name: "View All Departments" }).render();
+  console.log(logoText);
+  db.query("SELECT * FROM department", function (res, err) {
+    if (err) throw err;
+    console.table(res);
+  });
+}
+
+init();
