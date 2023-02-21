12461dfdf3ae7a1328742fef45b2b868576ca9b3
Mike <nicholasfierke97@gmail.com>
2023-02-21 15:33:39 -0500

    set up schema table for employee

--- db/schema.sql
+++ db/schema.sql
@@ -16,3 +16,16 @@ CREATE TABLE roles (
     REFERENCES department(id)
     ON DELETE SET NULL
 )
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES roles(id),
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON DELETE SET NULL
+)
